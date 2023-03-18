31c37c33e38eddd6d9b1ccfd065af0e35211e1fc
EC2 Default User <ec2-user@ip-172-31-22-208.ap-northeast-1.compute.internal>
2023-03-18 02:37:15 +0000

    スクリプトが正常に動かないバグを修正

--- app/javascript/script.js
+++ app/javascript/script.js
@@ -1,25 +1,29 @@
-// なぜか挙動がバグってスクロールしない
+// ターボリンクが原因で、正常にスクリプトが動かないバグを修正するには、
+//"turbolinks:load"の中にスクリプトを書く。
+document.addEventListener("turbolinks:load", function() {
+  // 処理内容
 
-/*global $*/$(function(){
-	$("#back a").on("click",function(event){
-		$("body, html").animate({
-			scrollTop: 0,
-		}, 400);
-		event.preventDefault();//aタグのリンク機能の無効化
-	});
+
+		/*global $*/$(function(){
+			$("#back a").on("click",function(event){
+				$("body, html").animate({
+					scrollTop: 0,
+				}, 400);
+				event.preventDefault();//aタグのリンク機能の無効化
+			});
+		});
+
+
+
+
+		$(function(){
+			$(".menu-trigger").on("click",function(event){
+				$(this).toggleClass("active");
+				$("#sp-menu").fadeToggle();
+				event.preventDefault();
+			});
 });
-
-
-
-
-$(function(){
-	$(".menu-trigger").on("click",function(event){
-		$(this).toggleClass("active");
-		$("#sp-menu").fadeToggle();
-		event.preventDefault();
-	});
-});
-
+})
 
 
 
